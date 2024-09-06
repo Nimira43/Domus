8837feb0bc037284d9a93c043ba8852393ad5b55
Nimira43 <n.rabone72@aol.com>
2024-09-06 21:15:24 +0100

    Create Navbar.jsx and styled the footer navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
-import Profile from './pages/Profile'
+// import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
-
 
 function App() {
   return (
@@ -19,6 +19,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
         </Routes>
+        <Navbar />
       </Router>
     </>
   )
